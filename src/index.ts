0045d0d5aef03b5646ef64b6ab8571a419457fbc
JaeilJo <whwodlf97@gmail.com>
2023-11-23 23:14:24 +0900

    feat : Add WebPack

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import './ts/contectInputStyleUpdater';
 import './ts/hamburgerButtonHandler';
 import './ts/intersectionObserver';
 import './ts/viewportHeightCalculator';
+
+import './ts/utils/functions/aboutSectionObserveCallback';
+import './ts/utils/functions/mainSectionObserveCallbakc';
+import './ts/utils/functions/projectSectionObserveCallback';
+import './ts/utils/functions/skillsSectionObserveCallback';
